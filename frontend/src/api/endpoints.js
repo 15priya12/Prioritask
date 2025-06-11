b3b67f5bb603e356cbb47f6af95baf6c7c33ece1
15priya12 <15priya12jain@gmail.com>
2025-06-11 19:07:57 +0530

    updating the deploy URLS

--- frontend/src/api/endpoints.js
+++ frontend/src/api/endpoints.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:3000/api/'
+const BASE_URL = 'https://prioritask-1h7j.onrender.com'
 const GET_URL = `${BASE_URL}todos`
 const POST_URL = `${BASE_URL}todos`
 const DELETE_URL = (id) => `${BASE_URL}todos/${id}`
