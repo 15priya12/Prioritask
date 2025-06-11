83c2b51db8591adc909165bd27c93a17bfbd8da3
15priya12 <15priya12jain@gmail.com>
2025-06-11 11:53:06 +0530

    changing the title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="App">
       <div className="app-container">
-      <h1 className='title'>Todo App</h1>
+      <h1 className='title'>PriorTask</h1>
       <AddTodo addTodo={addTodo}/>
        <TodoList todos={todos} deleteTodo = {deleteTodo}/>
       </div>
